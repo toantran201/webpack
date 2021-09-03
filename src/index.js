34436ded4d03ed75807210179c520b0bdfd9db19
Trần Toàn <trantoan@Trans-MacBook-Pro.local>
2021-09-03 17:26:44 +0700

    Section out put management start

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import _ from 'lodash'
-import './assert/styles/style.css'
-import Icon from './assert/images/icon.png'
-import Data from './assert/data/data.xml'
 
 function component() {
     const element = document.createElement("div");
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(["Hello", "webpack"], " ");
-    element.classList.add('hello');
-
-    const myIcon = new Image()
-    myIcon.src = Icon
-
-    element.appendChild(myIcon)
-
-    console.log(Data)
 
     return element;
 }
