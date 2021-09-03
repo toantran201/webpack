808bb72b8daf6d3e04a7321168d3be15356d78a1
Trần Toàn <trantoan@Trans-MacBook-Pro.local>
2021-09-03 19:49:06 +0700

    Finish output management

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import _ from 'lodash'
+import printMe from './print';
 
 function component() {
-    const element = document.createElement("div");
+    const element = document.createElement("div")
+    const btn = document.createElement('button')
 
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(["Hello", "webpack"], " ");
+    element.innerHTML = _.join(["Hello", "webpack"], " ")
 
-    return element;
+    btn.innerHTML = 'Click me and check the console!'
+    btn.onclick = printMe
+
+    element.appendChild(btn)
+    return element
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
 
 
 
