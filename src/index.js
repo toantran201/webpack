57db130203cea10939620ced9578aae5d154fcf7
Trần Toàn <trantoan@Trans-MacBook-Pro.local>
2021-09-03 20:21:27 +0700

    Finish development

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function component() {
     const btn = document.createElement('button')
 
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(["Hello", "webpack"], " ")
+    element.innerHTML = _.join(["Study", "webpack"], " ")
 
     btn.innerHTML = 'Click me and check the console!'
     btn.onclick = printMe
